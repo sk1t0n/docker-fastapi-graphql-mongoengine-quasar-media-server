9a13c787cc79d8c533ae94dc7aa49664fed46327
Anton Grabovsky <banderos893@gmail.com>
2020-10-16 18:41:38 +0300

    Added CORS to FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import graphene
 from starlette.graphql import GraphQLApp
 
 from db import connect, disconnect
 from api.graphql import Query, Mutation
 
+
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081"
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event('startup')
